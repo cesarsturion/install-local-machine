915da8b0d208848e05764a0b89367d460f27680a
Cesar Sturion <csturion@gmail.com>
2020-01-13 17:20:47 -0300

    Validando se docker e chrome já estão instalados e inclusão da instalação do drpbox

--- install-local-machine.sh
+++ install-local-machine.sh
@@ -30,15 +30,39 @@ then
 fi
 
 # instalando o docker
-if ! curl -fsSL https://get.docker.com | bash -
+docker=$(dpkg --get-selections | grep "docker-ce" )
+if ![ -n "$docker" ]
 then
-    echo "Não foi possível instalar o docker"
-    exit 1
+    if ! curl -fsSL https://get.docker.com | bash -
+        echo "Instalação do Docker finalizada"
+    then
+        echo "Não foi possível instalar o docker"
+        exit 1
+    fi
+else
+     echo "Docker ja instalado"
 fi
-echo "Instalação do Docker finalizada"
 
 # instalacao do chrome
-if ! 
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-rm google-chrome-stable_current_amd64.deb
+chrome=$(dpkg --get-selections | grep "chrome" )
+if ![ -n "$chrome" ]
+then
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    sudo dpkg -i google-chrome-stable_current_amd64.deb
+    rm google-chrome-stable_current_amd64.deb
+    echo "Instalação do Chrome finalizada"
+else
+     echo "Chrome ja instalado"
+fi
+
+dropbox=$(dpkg --get-selections | grep "dropbox" )
+if ![ -n "$dropbox" ]
+then
+    wget -O /usr/local/bin/dropbox "https://www.dropbox.com/download?dl=packages/dropbox.py"
+    chmod +x /usr/local/bin/dropbox
+    dropbox autostart y
+    echo "Instalação do DropBox finalizada"
+else
+     echo "Dropbox ja instalado"
+fi
+
